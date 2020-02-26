4cca3555f5e454010d87c2291fceec20bf95cd5a
wangyan4 <2236995139@qq.com>
2020-02-26 10:19:55 +0800

    add mocha

---
+++ test/calc.test.js
@@ -0,0 +1,15 @@
+const add = require('../calc');
+const expect = require('chai').expect;
+
+describe("加法函数单元测试",function(){
+  it('0 + 0 = 0',function(){
+    expect(add(0,0)).to.be.equal(0);
+  });
+
+  it('3 + 5 = 8',function(){
+    expect(add(3,5)).to.be.equal(8);
+  });
+  it("a + b = NaN",function(){
+    expect(isNaN(add(a,b))).to.be.ok;
+  })
+})
