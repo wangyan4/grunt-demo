f6a27aac921c78e777901ca329e2045afdcb33f5
wangyan4 <2236995139@qq.com>
2020-02-28 09:37:18 +0800

    add http-api-test

--- test/util.test.js
+++ test/util.test.js
@@ -1,11 +1,21 @@
-var expect = chai.expect;
-describe('矩形面积函数测试', () => {
-  it('area(2,10) = 20 ', () => {
-    var r = rectangle();
-    expect(r.area(2,10)).to.be.equal(20);
+const expect = require('chai').expect,
+      http = require('http');
+
+describe('矩形计算器 http api 接口测试', () => {
+  it('正确格式检测 ', (done) => {
+    http.get('http://localhost:8080/rectamgle?width=5&height=7'),function(){
+      var result='';
+      res.on('data',(chunk)=>{
+        result+= chunk;
+      });
+      res.on('end',()=>{
+        var rect = JSON.parse(result);
+        expect(rect.area).to.be.equal(35);
+        expect(rect.perimeter).to.be.equal(24);
+        done();
+      })
+    }
   });
-  it('perimeter(2,10) =  ', () => {
-    var r = rectangle();
-    expect(r.perimeter(2,10)).to.be.equal(20);
-  });
+  
 });
+
