c0c7f998d5bd090c7337912a9939f0ab5f7c71ff
wangyan4 <2236995139@qq.com>
2020-03-03 15:33:31 +0800

    add uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,12 @@
 /*global module :true*/
 module.exports = function (grunt) {
   grunt.initConfig({
-    cssmin: {
-      "index.min.css":"index.css"
+    uglify: {
+      "index.min.js":"index.js"
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['cssmin']); 
+  grunt.registerTask('default', ['uglify']); 
 };
