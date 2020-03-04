e848e29e6a106d7c1ccee2deed1260aa77fc6908
wangyan4 <2236995139@qq.com>
2020-03-04 08:29:29 +0800

    add imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,16 @@
 /*global module :true*/
 module.exports = function (grunt) {
   grunt.initConfig({
-    uglify: {
-      "index.min.js":"index.js"
+    imagemin: {                               
+      files: {
+        expand: true,
+        src: ['images/*.{png,jpg,gif}'],
+        dest: 'dist/'
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  grunt.registerTask('default', ['uglify']); 
+  grunt.registerTask('default', ['imagemin']);    
 };
