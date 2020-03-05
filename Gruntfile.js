74d495a74a9a674958060e2a51a1b3bb90742280
wangyan4 <2236995139@qq.com>
2020-03-05 16:19:46 +0800

    自定义构建任务

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,6 @@
 /*global module :true*/
 module.exports = function (grunt) {
-  grunt.initConfig({
-    sprite: {
-      files: {
-        src: 'icons/*.png',
-        dest: 'dist/bundle.png',
-        destCss: 'dist/sprites.css'
-      }
-    }
+  grunt.registerTask('default', "build task",function(){
+    console.log('build task');
   });
-
-  grunt.loadNpmTasks('grunt-spritesmith');
-
-  grunt.registerTask('default', ['sprite']);
 };
